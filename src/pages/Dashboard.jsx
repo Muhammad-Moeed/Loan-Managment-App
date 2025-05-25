a00b8ecc08a413f6f4a96558bd56f5804efc3e93
Muhammad-Moeed <m.moeedq497@gmail.com>
2025-05-25 11:59:40 +0500

    solve bugs during vercel deployment

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -42,7 +42,7 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import supabase from '../services/supabaseClient';
 import Chart from 'react-apexcharts';
-import '../app.css';
+import '../app.css'
 
 dayjs.extend(relativeTime);
 
